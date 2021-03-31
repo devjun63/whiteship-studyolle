72e8031cb56323ed82ebc656f9943a39d0cf5c5f
devjun63 <59526539+devjun63@users.noreply.github.com>
2021-04-01 01:26:25 +0900

    14. 프론트엔드 라이브러리 설정 & 15. 뷰 중복 코드 제거
    정적 리소스로 프론트엔드 라이브러리를 사용 & 빌드 고려
    thymeleaf fragment 사용하여 중복 코드 제거
    https://devjun.tistory.com/102?category=977941
    https://devjun.tistory.com/152?category=977941

--- src/main/java/com/studyolle/APP.java
+++ src/main/java/com/studyolle/APP.java
@@ -3,11 +3,17 @@ package com.studyolle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 @SpringBootApplication
 public class APP {
 
 	public static void main(String[] args) {
-
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(1,2);
 		SpringApplication.run(APP.class, args);
 	}
 
